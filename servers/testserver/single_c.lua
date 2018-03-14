9dd452cfa78b534cdc99c0a043c4f8be86e6a65c
wql <mydipan396@126.com>
2018-03-14 12:25:12 +0800

    watchdog

--- servers/testserver/single_c.lua
+++ servers/testserver/single_c.lua
@@ -2,7 +2,7 @@ local skynet = require "skynet"
 require "skynet.manager"
 
 local ok,testitem = pcall(require,skynet.getenv("testitem") or nil)
-print(skynet.getenv("testitem"))
+print(testitem)
 
 local MAX_CONNECT_TIME = 300
 
