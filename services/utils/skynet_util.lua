eba160f32ddb8954e990bb1d4180566b4a0d9706
wql <mydipan396@126.com>
2018-03-14 18:17:49 +0800

    watchdog

--- services/utils/skynet_util.lua
+++ services/utils/skynet_util.lua
@@ -11,24 +11,21 @@ function util.lua_docmd(cmdhandler,session,cmd,...)
 		local f
 		if handler_type == "table" then
 			f = cmdhandler[cmd]
-                        print(f)
 		elseif handler_type == "function" then
 			f = cmdhandler
 		end
-        end
         
-        if not f then
-        	local l_des = string.format("Unknow command %s", tostring(cmd))
-        	res = {false,l_des}
-                print(l_des)
-        else
-                print("call cmd")
-        	local l_rst = pcall(f,...)
-        	if not l_rst then
-        		res = {false,"raise error"}
-        	else
-        		res = {l_rst[2],l_rst[3],l_rst[4]}
-        	end
+                if not f then
+                	local l_des = string.format("Unknow command %s", tostring(cmd))
+                	res = {false,l_des}
+                else
+                	local l_rst = pcall(f,...)
+                	if not l_rst then
+                		res = {false,"raise error"}
+                	else
+                		res = {l_rst[2],l_rst[3],l_rst[4]}
+                	end
+                end
         end
 
         if session == 0 then
