d4c42566693eac7f270e8b1468d611943ad7d7af
wql <mydipan396@126.com>
2018-03-14 11:37:50 +0800

    watchdog

---
+++ servers/testserver/main.lua
@@ -0,0 +1,11 @@
+
+local skynet = require "skynet"
+
+-- 启动服务(启动函数)
+skynet.start(function()
+    -- 启动函数里调用Skynet API开发各种服务
+    print("======Server start=======")
+    
+    
+    skynet.exit()
+end)
