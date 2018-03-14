25e6d969810d752f7ecb6148ca6a50f6f96c9008
wql <mydipan396@126.com>
2018-03-14 11:49:01 +0800

    watchdog

--- servers/testserver/main.lua
+++ servers/testserver/main.lua
@@ -7,7 +7,7 @@ skynet.start(function()
     -- 启动函数里调用Skynet API开发各种服务
     print("======Server start=======")
     skynet.newservice("console")
-    skynet.newservice("debug_console")
+    skynet.newservice("debug_console",debugport)
     skynet.newservice("mutil_c")    
     
     skynet.exit()
