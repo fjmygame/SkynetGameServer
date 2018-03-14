d843af3b57ba145d0861ab910ea0f16c882076f9
wql <mydipan396@126.com>
2018-03-14 11:46:26 +0800

    watchdog

--- servers/testserver/main.lua
+++ servers/testserver/main.lua
@@ -1,11 +1,14 @@
 
 local skynet = require "skynet"
+local debugport = tonumber(skynet.getenv("debugport") or "8888")
 
 -- 启动服务(启动函数)
 skynet.start(function()
     -- 启动函数里调用Skynet API开发各种服务
     print("======Server start=======")
-    
+    skynet.newservice("console")
+    skynet.newservice("debug_console")
+    skynet.newservice("mutil_c")    
     
     skynet.exit()
 end)
