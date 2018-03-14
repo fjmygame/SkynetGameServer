edcd7489a47af08fd2d78b95f5c543e3e58c585a
wql <mydipan396@126.com>
2018-03-14 11:09:52 +0800

    logger,preload

--- servers/main.lua
+++ servers/main.lua
@@ -6,6 +6,13 @@ skynet.start(function()
     -- 启动函数里调用Skynet API开发各种服务
     print("======Server start=======")
     
+    skynet.newservice("logservice")
     skynet.newservice("debug_console",8001)
+    local watchdog = skynet.newservice("watchdog")
+    skynet.call(watchdog,"lua", "start",{
+          port=8200,
+          maxclient=100,
+          nodelay=true
+          })
     skynet.exit()
 end)
---
+++ servers/preload/common_preload.lua
@@ -0,0 +1,2 @@
+--package.cpath=""..package.cpath
+package.path="SkynetGameServer/services/persistent/log/?.lua;"..package.path
