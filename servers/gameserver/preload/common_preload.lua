29fb6659322405b06575d16bdb1ed3e154b51e31
wql <mydipan396@126.com>
2018-03-15 16:02:33 +0800

    agent

--- servers/gameserver/preload/common_preload.lua
+++ servers/gameserver/preload/common_preload.lua
@@ -1,3 +1,4 @@
 --package.cpath=""..package.cpath
 package.path="../services/persistent/log/?.lua;"..package.path
+package.path="../services/agent/?.lua;"..package.path
 package.path="../services/utils/?.lua;"..package.path
