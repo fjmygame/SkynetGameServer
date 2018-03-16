81bd32f0e560e938160692ac3d974b395e86eae9
wql <mydipan396@126.com>
2018-03-16 15:53:50 +0800

    protoloader

---
+++ servers/gameserver/protoloader.lua
@@ -0,0 +1,14 @@
+
+local skynet = require "skynet"
+local sprotoparser = require "sprotoparser"
+local sprotoloader = require "sprotoloader"
+local proto = require "proto"
+
+local gamename = skynet.getenv("gamename") or nil
+
+skynet.start(function()
+	proto:init(gamename)
+	sprotoloader.save(proto.c2s, 1)
+	sprotoloader.save(proto.s2c, 2)
+	-- don't call skynet.exit() , because sproto.core may unload and the global slot become invalid
+end)
