7e87a621ddceb905400496055917e0db9c3ea52c
wql <mydipan396@126.com>
2018-03-27 15:17:07 +0800

    agent

--- servers/gameserver/preload/common_preload.lua
+++ servers/gameserver/preload/common_preload.lua
@@ -1,5 +1,6 @@
 --package.cpath=""..package.cpath
 package.path="../services/persistent/log/?.lua;".."../services/agent/?.lua;"..package.path
 package.path="../services/utils/?.lua;".."../services/common/?.lua;"..package.path
+package.path="../services/physical/cluster_mgr/?.lua;"..package.path
 package.path="lualib/?.lua;lualib/?/init.lua;"..package.path
 package.path="../proto/?.lua;"..package.path
