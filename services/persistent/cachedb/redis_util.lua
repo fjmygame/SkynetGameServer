f3f773d2589dd96cec8fc2bf6cc1eae795396574
wql <mydipan396@126.com>
2018-03-28 16:39:18 +0800

    agent

---
+++ services/persistent/cachedb/redis_util.lua
@@ -0,0 +1,25 @@
+local skynet = require "skynet"
+local redis = require "redis"
+
+require "skynet.manager"
+
+local redis_util = {}
+
+function redis_util.init()
+	
+end
+
+
+function redis_util.query_with_gsub(redisstr,args,redispt,pattern)
+	-- body
+end
+
+function redis_util.querycmd(...)
+	-- body
+end
+
+function redis_util.excute_with_gsub(redisstr,args,redispt,pattern)
+	-- body
+end
+
+return redis_util
