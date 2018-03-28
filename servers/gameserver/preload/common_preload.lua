adb912b446ed891c201b4c297a69e554a23160c6
wql <mydipan396@126.com>
2018-03-28 16:35:47 +0800

    agent

--- servers/gameserver/preload/common_preload.lua
+++ servers/gameserver/preload/common_preload.lua
@@ -5,3 +5,4 @@ package.path="../services/physical/cluster_mgr/?.lua;"..package.path
 package.path="../services/persistent/cachedb/?.lua;"..package.path
 package.path="lualib/?.lua;lualib/?/init.lua;"..package.path
 package.path="../proto/?.lua;"..package.path
+package.path="../dbcache/?.lua;"..package.path
