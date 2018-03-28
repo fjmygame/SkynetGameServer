5735cdd297351843793fac42b9c04e815a823ca7
wql <mydipan396@126.com>
2018-03-28 16:30:07 +0800

    agent

--- services/utils/futil.lua
+++ services/utils/futil.lua
@@ -8,7 +8,7 @@ function futil.split(s, sp)
     local len = 0  
     while true do  
         len = string.find(temp, sp)  
-        if len ~= nil then  
+        if temp and len ~= nil then  
             local result = string.sub(temp, 1, len-1)  
             temp = string.sub(temp, len+1)  
             table.insert(res, result)  
