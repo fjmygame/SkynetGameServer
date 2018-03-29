53148e385e47b6cc6a6ebeff5db68c994444f06c
wql <mydipan396@126.com>
2018-03-29 10:09:44 +0800

    agent

---
+++ dbcache/divided_conf.lua
@@ -0,0 +1,12 @@
+
+local divide_conf = {
+	db_player = {}
+}
+
+divide_conf.db_player.logonlog = {
+	divi_key = "login_time",
+	divi_base = 1,   --按月
+	divi_type = 1;   --按日期
+}
+
+return divide_conf
--- services/persistent/cachedb/proxyloader.lua
+++ services/persistent/cachedb/proxyloader.lua
@@ -1,6 +1,9 @@
 local skynet = require "skynet"
+local divided_conf = require "divided_conf"
+--local libsql_split = require "libsql_split"
 
 --加载db分表配置
 skynet.start(function() 
-    
+    -- libsql_split.load_divide_conf(divided_conf)
+    -- libsql_split.travel_divide_conf(divided_conf)
 end)
