18cc6829dacc9cc5f095cbc5f1116e2559b57234
wql <mydipan396@126.com>
2018-03-29 14:33:47 +0800

    agent

--- servers/gameserver/main.lua
+++ servers/gameserver/main.lua
@@ -2,12 +2,15 @@
 local skynet = require "skynet"
 local cache_util = require "cache_util"
 
+local logfilename = skynet.getenv("logfilename") or skynet.getenv("nodename") or "unknownsvr"
+
 -- 启动服务(启动函数)
 skynet.start(function()
     -- 启动函数里调用Skynet API开发各种服务
     print("======Server start=======")
     skynet.newservice("clustermgr")
     skynet.newservice("logservice")
+    skynet.call(".logservice", "lua", "set_log_file", logfilename)
     skynet.newservice("debug_console",8001)
 
     skynet.uniqueservice("protoloader")
